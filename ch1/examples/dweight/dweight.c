f6aadecb5baa5036752f9630f9c8e1c48f04d4d2
rtrom <rhett.trom@pm.me>
2023-07-12 13:19:09 -0500

    Added user input

--- ch1/examples/dweight/dweight.c
+++ ch1/examples/dweight/dweight.c
@@ -3,13 +3,15 @@
 int main(void) {
   int height, length, width, volume, weight;
 
-  height = 8;
-  length = 12;
-  width = 10;
+  printf("Enter the height of the box: \n");
+  scanf("%d", &height);
+  printf("Enter the length of the box: \n");
+  scanf("%d", &length);
+  printf("Enter the width of the box: \n");
+  scanf("%d", &width);
   volume = height * length * width;
   weight = (volume + 165) / 166;
 
-  printf("Dimensions: %dx%dx%d\n", length, width, height);
   printf("Volume (cubic inches): %d\n", volume);
   printf("Dimensional weight (pounds): %d\n", weight);
 
